2b7d21e6084f4d60b10e9d2cb6fa65c00e2c0bc7
Thiago <thiago.miotti@6place.com.br>
2022-10-03 09:45:06 -0300

    First commit

---
+++ src/pages/app/App.js
@@ -0,0 +1,23 @@
+import './App.css';
+import ImagemTelaLogin from "../../assets/imagemTelaLogin.svg";
+import LogoEspm from "../../assets/LogoESPMFundoVermelho.svg";
+import LogoMicrosoft from "../../assets/LogoMicrosoft.svg";
+
+function App() {
+  return (
+    <div className='containerPai'>
+      <div className='containerFilho'>
+        <div className='metade1'>
+          <img src={ImagemTelaLogin} alt='imagemTelaLogin'/>
+          <p>BEM VINDO AO ESPM <span>academics</span></p>
+        </div>
+        <div className='metade2'>
+          <img src={LogoEspm} alt="Logo ESPM"/>
+          <button> <img src={LogoMicrosoft} alt='logo microsoft'/> Entrar com Microsoft</button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
